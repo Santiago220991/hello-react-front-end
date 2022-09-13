ed8c380d05cbccaa1124c29f681ca2ca58f841c2
Santiago <alexander.cardenas10@outlook.com>
2022-09-13 15:57:56 -0500

    Check linters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Greeting from "./components/greeting";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Greeting from './components/greeting';
 import Store from './redux/configureStore';
+
 function App() {
   return (
     <Provider store={Store}>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={ <Greeting /> } />
+          <Route exact path="/" element={<Greeting />} />
         </Routes>
       </BrowserRouter>
     </Provider>
